475b8be178369649d34ea570b1d51205ef62bbde
윤동영 <ehddud9608@naver.com>
2025-03-25 12:36:05 +0900

    [Feat]: Slot에 아이템 없을 시 슬롯삭제기능추가
    Slot에 아이템 없을 시 슬롯삭제기능추가

--- Assets/01_Scripts/01_02 UI/UIInventory.cs
+++ Assets/01_Scripts/01_02 UI/UIInventory.cs
@@ -6,7 +6,7 @@ public class UIInventory : MonoBehaviour
 {
     [SerializeField] private UIslot slotPrefab;
     [SerializeField] private Transform slotParent;
-    private List<UIslot > slots = new List<UIslot>();
+    private List<UIslot > slotsList = new List<UIslot>();
 
     [Header("버튼")]
     public Button mainUIBtn;
@@ -22,7 +22,22 @@ public class UIInventory : MonoBehaviour
         for(int i = 0; i < allSlots; i++)
         {
             UIslot newSlot = Instantiate(slotPrefab, slotParent); //새로운 슬롯을 만들어서 slotParent에 넣고 리스트에 추가
-            slots.Add(newSlot);
+            slotsList.Add(newSlot);
+        }
+    }
+    public void SetItemData(List<Item> inventory) // 인벤토리에 저장된 데이터를 UI로 반영하기 위해서
+    {
+        for(int i=0; i<inventory.Count; i++) // 데이터가 인벤토리의 총 갯수보다 작아질 때까지
+        {
+            if(i<inventory.Count)
+            {
+                slotsList[i].SetItem(inventory[i]); //UI에 데이터를 추가해주고
+            }
+            else //초과했다면
+            {
+                //삭제를 해줘야함.
+                slotsList[i].ClearSlot();
+            }
         }
     }
 }
--- Assets/01_Scripts/01_02 UI/UIslot.cs
+++ Assets/01_Scripts/01_02 UI/UIslot.cs
@@ -9,21 +9,28 @@ public class UIslot : MonoBehaviour
     [SerializeField] private Image icon;
     [SerializeField] private TextMeshProUGUI quantityText; //인벤토리에 있는 아이템의 개수
 
-    private Item item;
     private int curQuantity;
+    private Item currentItem;
+
 
     public void SetItem(Item newItem, int quantity = 1)
     {
-        item = newItem; //새로운 아이템을 할당
+        currentItem = newItem; //새로운 아이템을 할당
         curQuantity = quantity;
         UpdateSlot();
     }
-
+    public void ClearSlot() //아이템이 저장되어 있지 않은 슬롯을 삭제시켜주는 메서드
+    {
+        currentItem = null;
+        icon.sprite = null;
+        icon.enabled = false;
+        quantityText.text = ""; 
+    }
     public void UpdateSlot()
     {
-        if (item != null)  //갖고있는 아이템이라면
+        if (currentItem != null)  //갖고있는 아이템이라면
         {
-            icon.sprite = item.Image;
+            icon.sprite = currentItem.Image;
             icon.enabled = true;  // 갖고있는 아이템 이미지 활성화
             quantityText.text = curQuantity > 1 ? curQuantity.ToString() : ""; //삼항연산자를 활용하여 갖고있는 아이템이 1보다 크다면 개수를 표시
         }
